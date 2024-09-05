5b61619f3c30c663551cbf425d84318787a48246
dahunsi-dami <gbemigadare@gmail.com>
2024-09-05 04:06:15 +0100

    Add method to decode Base64 str w/ other requirements.

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Module for user credential management. """
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -25,3 +26,24 @@ class BasicAuth(Auth):
         if not authorization_header.startswith('Basic '):
             return None
         return authorization_header[6:]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:  # noqa: E501
+        """
+        Gets decoded value of a Base64 string.
+
+        Returns:
+            - None if base64_authorization_header is None.
+            - None if base64_authorization_header is not a string.
+            - None if base64_authorization_header not valid Base64,
+            using try/except block.
+            - Otherwise, decoded value as UTF8 string w/ decode('utf-8').
+        """
+        if base64_authorization_header is None:
+            return None
+        if not isinstance(base64_authorization_header, str):
+            return None
+        try:
+            decode_val = base64.b64decode(base64_authorization_header)
+            return decode_val.decode('utf-8')
+        except (base64.binascii.Error, UnicodeDecodeError):
+            return None
