4f5672dd552ae83000248df96911765422830086
dahunsi-dami <gbemigadare@gmail.com>
2024-09-06 01:33:48 +0100

    Use prev methods to do basic auth in current_user().

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -104,3 +104,29 @@ class BasicAuth(Auth):
             return user
         except Exception:
             return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """
+        Overloads Auth & gets User instance for a request.
+
+        Returns:
+            - User instance for a request.
+        """
+        auth_header = self.authorization_header(request)
+        if auth_header is None:
+            return None
+
+        base64_auth = self.extract_base64_authorization_header(auth_header)
+        if base64_auth is None:
+            return None
+
+        decoded_auth = self.decode_base64_authorization_header(base64_auth)
+        if decoded_auth is None:
+            return None
+
+        user_email, user_pwd = self.extract_user_credentials(decoded_auth)
+        if user_email is None or user_pwd is None:
+            return None
+
+        user_inst = self.user_object_from_credentials(user_email, user_pwd)
+        return user_inst
