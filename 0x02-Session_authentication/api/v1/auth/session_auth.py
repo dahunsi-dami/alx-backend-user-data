9bfec5cf2fd439b6d9e283b2a813117e374a1fb1
dahunsi-dami <gbemigadare@gmail.com>
2024-09-09 16:11:58 +0000

    Add documentation to session_auth.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
+"""Session authentication module."""
 from api.v1.auth.auth import Auth
 
 
 class SessionAuth(Auth):
-    """Class for session authentication mechanism."""
+    """
+    Class for session authentication mechanism.
+
+    It inherits from the Auth class and builds on-
+    -the authentication methods in Auth.
+    """
     pass
