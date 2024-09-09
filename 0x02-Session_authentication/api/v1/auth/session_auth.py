e649d9ee9196907270c5891e92d29462cec5ef61
dahunsi-dami <gbemigadare@gmail.com>
2024-09-09 20:01:31 +0000

    Add method to SessionAuth class to get user ID as specced.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -35,3 +35,18 @@ class SessionAuth(Auth):
         self.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Creates an instance method.
+
+        Returns:
+            - None if session_id is None.
+            - None if session_id not a string.
+            - value (user ID) for key, session_id in dict called
+            user_id_by_session_id.
+        Must use .get() built-in to access value w/ key in dict.
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
