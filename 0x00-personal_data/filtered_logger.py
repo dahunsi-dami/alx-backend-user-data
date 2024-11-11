89f56ec787fb34464d92cdab023aa036d0860ff7
dahunsi-dami <gbemigadare@gmail.com>
2024-11-12 00:28:11 +0100

    Set record.msg directly in format method so custom FORMAT str is applied correctly.

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -48,10 +48,10 @@ class RedactingFormatter(logging.Formatter):
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        record.message = filter_datum(
+        record.msg = filter_datum(
             self.fields,
             self.REDACTION,
             record.getMessage(),
-            self.SEPARATOR
+            self.SEPARATOR + ' '
         )
         return super().format(record)
